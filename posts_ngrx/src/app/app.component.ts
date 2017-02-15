b7358fa6e7863d76669a53cf3cb96855d1e112e8
Carlos Chitty <chitty.carlosj@gmail.com>
2017-02-14 19:59:59 -0500

    Use service for posts

--- posts_ngrx/src/app/app.component.ts
+++ posts_ngrx/src/app/app.component.ts
@@ -1,32 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-const POSTS: Post[] = [
-  { id: 11, status: 1, title: 'Mr. Nice' },
-  { id: 12, status: 1, title: 'Narco' },
-  { id: 13, status: 1, title: 'Bombasto' },
-  { id: 14, status: 1, title: 'Celeritas' },
-  { id: 15, status: 1, title: 'Magneta' },
-  { id: 16, status: 1, title: 'RubberMan' },
-  { id: 17, status: 1, title: 'Dynama' },
-  { id: 18, status: 1, title: 'Dr IQ' },
-  { id: 19, status: 1, title: 'Magma' },
-  { id: 20, status: 1, title: 'Tornado' }
-];
+import { Post } from './post';
+import { PostService } from './post.service';
 
-export class Post {
-  id: number;
-  title: string;
-  status: number;
-}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [PostService]
 })
-export class AppComponent {
-  public posts = POSTS;
+export class AppComponent implements OnInit {
   title = 'The Blog';
   selectedPost: Post;
+  posts: Post[];
+
+  constructor(private postService: PostService) { }
+
+  ngOnInit(): void {
+    this.getPosts();
+  }
+
+
+  getPosts(): void {
+    this.posts = this.postService.getPosts();
+  }
+
 
   onSelect(post: Post): void {
     this.selectedPost = post;
