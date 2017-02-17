b92705a7350c0a0cb2664a7db4ee718b5963144f
Carlos Chitty <chitty.carlosj@gmail.com>
2017-02-17 14:06:10 -0500

    Use ngrx/store devtools and log-monitor

--- posts_ngrx/src/app/app.module.ts
+++ posts_ngrx/src/app/app.module.ts
@@ -3,9 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreLogMonitorModule, useLogMonitor } from '@ngrx/store-log-monitor';
 
 import { posts } from './posts';
 import { AppComponent } from './app.component';
+
+export function instrumentOptions() {
+  return {
+    monitor: useLogMonitor({ visible: true, position: 'right' })
+  };
+}
 
 @NgModule({
   declarations: [
@@ -15,7 +23,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    StoreModule.provideStore({ posts })
+    StoreModule.provideStore({ posts }),
+    StoreDevtoolsModule.instrumentStore(instrumentOptions),
+    StoreLogMonitorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
